dcd44979a6c7dcfce732c0106d894e0740f480ac
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2025-08-27 14:17:03 +0200

    wip: removed for the moment

--- ui/main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-import simpy
-from ui.main_window import MainWindow # Corrected class name
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = MainWindow(root) # Corrected class name
-    root.mainloop()
